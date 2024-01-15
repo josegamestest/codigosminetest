49bde63019f758b335eff59c708f8e22eca47a57
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2024-01-14 21:53:59 -0300

    Update Chat_msg_colorida.lua
    corrigindo codigo nao da resultado

--- Chat_msg_colorida.lua
+++ Chat_msg_colorida.lua
@@ -1,8 +1,11 @@
-on_rightclick = function(self, clicker, rot, texto, jogador)
-    if not clicker or not clicker:is_player() then return end
-    jogador=clicker:get_player_name()
-    
-minetest.chat_send_player(jogador, "esta e uma mensagem sem cor"}
-end,
-minetest.chat_send_player(jogador,minetest.colorize("#7FFFD470","Esta e uma frase colorida"))
-minetest.chat_send_player(jogador,minetest.colorize("#AA000070","Esta e outra frase colorida"))
+minetest.register_chatcommand("cor", {
+    params = "<playername>",
+    description = "Exibe informações sobre um jogador",
+    func = function(name, param)
+            jogador = name
+            minetest.chat_send_all(jogador.." esta dizendo")
+            minetest.chat_send_player(jogador, "Esta é uma mensagem sem cor")
+            minetest.chat_send_player(jogador, minetest.colorize("#7FFFD470", "Esta é uma frase colorida"))
+            minetest.chat_send_player(jogador, minetest.colorize("#AA000070", "Esta é outra frase colorida"))
+        --end
+end})
